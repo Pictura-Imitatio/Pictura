aea6847a003a38fe6b719916113a6300f73156d8
James Eginton <jwe66@cornell.edu>
2023-08-13 17:02:52 -0500

    pog shit

--- src/image.rs
+++ src/image.rs
@@ -1,36 +1,42 @@
-use screenshots;
-use std::num;
+use screenshots::Compression;
+use std::fs;
 
 // Struct for pixels on the screen.
 pub struct Point {
-    pub x: u32,
-    pub y: u32,
+    pub x: i32,
+    pub y: i32,
 }
 
 fn screenshot(xy1: Point, xy2: Point) -> screenshots::Image {
     let display_info = screenshots::DisplayInfo::all().unwrap()[0];
     let capture = screenshots::Screen::new(&display_info);
-    let width: u32 = num::abs(xy2.x - xy1.x);
-    let height: u32 = num::abs(xy2.y - xy1.y);
+    let width: u32 = (xy2.x - xy1.x).abs() as u32;
+    let height: u32 = (xy2.y - xy1.y).abs() as u32;
     let corner = Point {
-        x: if xy1.x > xy2.x {xy1.x} else {xy2.x},
-        y: if xy1.y > xy2.y {xy1.y} else {xy2.y},
+        x: if xy1.x < xy2.x {xy1.x} else {xy2.x},
+        y: if xy1.y < xy2.y {xy1.y} else {xy2.y},
     };
     capture.capture_area(corner.x, corner.y, width, height).unwrap()
 }
 
 fn main() {
     let xy1 = Point {
-        x: 0,
-        y: 0,
+        x: 5,
+        y: 5,
     };
     let xy2 = Point {
-        x: 1080,
-        y: 1920,
+        x: 1070,
+        y: 1910,
     };
 
     let image = screenshot(xy1, xy2);
     let buffer = image.to_png(Compression::Fast).unwrap();
     let compressed_buffer = image.to_png(Compression::Best).unwrap();
+    
+    fs::write(format!("target-2.png"), buffer).unwrap();
 
+    fs::write(
+        format!("target-2-compressed.png"), 
+        compressed_buffer
+    ).unwrap();
 }
