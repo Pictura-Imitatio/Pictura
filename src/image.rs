9e6210acc8b9bee3286cac01154855e2a6ad502d
James Eginton <jwe66@cornell.edu>
2023-08-19 22:19:10 -0500

    expanding on the core functionality of the image run file to accept multiple compression algorithms.

--- src/image.rs
+++ src/image.rs
@@ -1,5 +1,4 @@
 use screenshots::{Compression, Screen};
-use std::fs;
 
 // Struct for pixels on the screen.
 pub struct Point {
@@ -7,24 +6,36 @@ pub struct Point {
     pub y: i32,
 }
 
-
-fn screenshot(xy1: Option<Point>, xy2: Option<Point>, screen: Option<i32>) -> screenshots::Image {
-    let display_info = screenshots::DisplayInfo::all().unwrap();
+/* TODO: 
+ *  - grab screens from point
+ *  - attempt to allow a square of 2 monitors
+ */
+fn screenshot(xy1: Option<Point>, xy2: Option<Point>, screen: Option<usize>) -> Vec<screenshots::Image> {
     
-    if (screen.is_none()) {
+    if screen.is_none() {
+        let screens = Screen::all().unwrap();
+        let mut images = Vec::new();
+        for capture in screens {
+            images.push(capture.capture().unwrap());
+        }
+        images
     }
-    let capture = screenshots::Screen::new(&display_info);
-    let xy1 = xy1.unwrap();
-    let xy2 = xy2.unwrap();
+    else {
+        let display_info = screenshots::DisplayInfo::all().unwrap()[screen.unwrap()];
 
-    
-    let width: u32 = (xy2.x - xy1.x).abs() as u32;
-    let height: u32 = (xy2.y - xy1.y).abs() as u32;
-    let corner = Point {
-        x: if xy1.x < xy2.x {xy1.x} else {xy2.x},
-        y: if xy1.y < xy2.y {xy1.y} else {xy2.y},
-    };
-    capture.capture_area(corner.x, corner.y, width, height).unwrap()
+        let capture = screenshots::Screen::new(&display_info);
+        let xy1 = xy1.unwrap();
+        let xy2 = xy2.unwrap();
+
+        let width: u32 = (xy2.x - xy1.x).abs() as u32;
+        let height: u32 = (xy2.y - xy1.y).abs() as u32;
+        
+        let corner = Point {
+            x: if xy1.x < xy2.x {xy1.x} else {xy2.x},
+            y: if xy1.y < xy2.y {xy1.y} else {xy2.y},
+        };
+        vec![capture.capture_area(corner.x, corner.y, width, height).unwrap()]
+    }
 }
 
 fn read() -> i32 {
@@ -32,28 +43,24 @@ fn read() -> i32 {
 }
 
 pub fn run(compression: Option<String>, 
-           screen: Option<i32>,
-           bounds: (Option<Point>, Option<Point>)) -> Result<Vec<u8, Global>, EncodingError> {
+           screen: Option<usize>,
+           bounds: (Option<Point>, Option<Point>)) -> Vec<u8> {
 
-    let image = screenshot(bounds.0, bounds.1, screen);
-    let mut compressed_buffer: Result<Vec<u8, Global>, EncodingError>;
-    match &**compression.unwrap_or(*"").to_lowercase() {
-        "Best"  => {
-            let compressed_buffer = image.to_png(Compression::Best).unwrap();
-        },
-        "Rle"  => {
-            let compressed_buffer = image.to_png(Compression::Rle).unwrap();
-        },
-        "Huffman"  => {
-            let compressed_buffer = image.to_png(Compression::Huffman).unwrap();
-        },
-        "Fast"  => {
-            let compressed_buffer = image.to_png(Compression::Fast).unwrap();
-        },
+    let images = screenshot(bounds.0, bounds.1, screen);
+    let mut compressed_buffers = Vec::new();
+    for image in images {
+        match &*compression.unwrap_or_default().to_lowercase() {
+            "Best"  => {
+                compressed_buffers.push(image.to_png(Compression::Best).unwrap());
+            },
+            "Fast"  => {
+                compressed_buffers.push(image.to_png(Compression::Fast).unwrap());
+            },
 
-        _   => {
-            let compressed_buffer = image.to_png(Compression::Default).unwrap();
+            _   => {
+                compressed_buffers.push(image.to_png(Compression::Default).unwrap());
+            }
         }
     }
-    compressed_buffer
+    compressed_buffers
 }
