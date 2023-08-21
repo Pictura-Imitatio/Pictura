3702a4458d8aa2cea4fef625e7ba041e6a5bb2c4
James Eginton <jwe66@cornell.edu>
2023-08-20 20:44:32 -0500

    potential multimonitor drag support

--- src/image.rs
+++ src/image.rs
@@ -6,48 +6,91 @@ pub struct Point {
     pub y: i32,
 }
 
+fn global_to_local(point: &Point, screen: Screen) -> Point {
+    Point {
+        x: point.x - screen.display_info.x,
+        y: point.y - screen.display_info.y,
+    }
+}
+
 /* TODO: 
- *  - grab screens from point
- *  - attempt to allow a square of 2 monitors
+ *  TODO: - grab screens from point
+ *  TODO: - attempt to allow a square of 2 monitors
+ *  DONE: - make point order and position ambigious (i.e. (bl,tr), or (tl,br), etc)
  */
-fn screenshot(xy1: Option<Point>, xy2: Option<Point>, screen: Option<usize>) -> Vec<screenshots::Image> {
-    
-    if screen.is_none() {
+fn screenshot(global_coordinates: (Option<Point>, Option<Point>)) -> Vec<screenshots::Image> {
+    if global_coordinates.0.is_none() && global_coordinates.1.is_none() {
+    //if screen.is_none() {
         let screens = Screen::all().unwrap();
         let mut images = Vec::new();
         for capture in screens {
-            images.push(capture.capture().unwrap());
+            let cap = capture.capture().unwrap();
+            images.push(cap);
         }
         images
     }
+
+    /* TODO:
+     *  TODO: - what about the middle monitor
+     *          -- think about comparing the global coordinates of all screens to the range of tlbr
+     * 
+     *  */
     else {
-        let display_info = screenshots::DisplayInfo::all().unwrap()[screen.unwrap()];
-
-        let capture = screenshots::Screen::new(&display_info);
-        let xy1 = xy1.unwrap();
-        let xy2 = xy2.unwrap();
-
-        let width: u32 = (xy2.x - xy1.x).abs() as u32;
-        let height: u32 = (xy2.y - xy1.y).abs() as u32;
-        
-        let corner = Point {
-            x: if xy1.x < xy2.x {xy1.x} else {xy2.x},
-            y: if xy1.y < xy2.y {xy1.y} else {xy2.y},
+        let global_coordinates = (global_coordinates.0.unwrap(), global_coordinates.1.unwrap());
+        let global_tl = Point { // The top left of the rectangle created by the global coordinates
+            x: if global_coordinates.0.x < global_coordinates.1.x { global_coordinates.0.x } else { global_coordinates.1.x },
+            y: if global_coordinates.0.y < global_coordinates.1.y { global_coordinates.0.y } else { global_coordinates.1.y },
         };
-        vec![capture.capture_area(corner.x, corner.y, width, height).unwrap()]
+
+        let global_br = Point {  // The bottom right of the rectangle created by the global coordinates
+            x: if global_coordinates.0.x < global_coordinates.1.x { global_coordinates.1.x } else { global_coordinates.0.x },
+            y: if global_coordinates.0.y < global_coordinates.1.y { global_coordinates.1.y } else { global_coordinates.0.y }
+        };
+
+
+        let screen_tl = Screen::from_point(global_tl.x, global_tl.y).unwrap();   // Screen that contains the top left coodinate
+        let screen_br = Screen::from_point(global_br.x, global_br.y).unwrap();   // Screen that contains the bottom right coordinate
+        let local_tl  = global_to_local(&global_tl, screen_tl);                                  // Top left in local coordinates
+        let local_br  = global_to_local(&global_br, screen_br);                                  // Bottom right in local coordinates
+
+        if screen_tl.display_info.id != screen_br.display_info.id {
+        /* TODO:
+         *  - 
+         */
+            let local_tl_width  = screen_tl.display_info.width - local_tl.x as u32;
+            let local_tl_height = (local_tl.y - global_to_local(&global_br, screen_tl).y) as u32;
+
+            let local_br_tl     = Point { // the local top left corner of the rectangle on the br screen
+                x: 0,
+                y: global_to_local(&global_tl, screen_br).y,
+            };
+            let local_br_width  = local_br.x as u32;
+            let local_br_height = (local_br.y - local_br_tl.y) as u32;
+
+            vec![screen_tl.capture_area(local_tl.x,    local_tl.y,     local_tl_width, local_tl_height).unwrap(),
+                 screen_br.capture_area(local_br_tl.x, local_br_tl.y,  local_br_width, local_br_height).unwrap()]
+        }
+
+        else {
+            /* TODO:
+             *  DONE: - convert global to local
+             *  TODO: - 
+             */
+            let width:u32  = (local_br.x - local_tl.x) as u32;
+            let height:u32 = (local_br.y - local_tl.y) as u32;
+            println!("{} {}/{} {}\n{width}/{height}\n{screen_tl:?}", local_tl.x, local_tl.y, local_br.x, local_br.y);
+            vec![screen_tl.capture_area(local_tl.x, local_tl.y, width, height).unwrap()]
+        }
+        
     }
+
 }
 
-fn read() -> i32 {
-    1i32
-}
-
-pub fn run(compression: Option<String>, 
-           screen: Option<usize>, 
+pub fn run(compression: Option<String>,
            bounds: (Option<Point>, Option<Point>)) 
             -> Vec<Vec<u8>> {
 
-    let images = screenshot(bounds.0, bounds.1, screen);
+    let images = screenshot((bounds.0, bounds.1));
     let mut compressed_buffers = Vec::new();
     for image in images {
         match &*compression.clone().unwrap_or_default().to_lowercase() {
--- src/main.rs
+++ src/main.rs
@@ -64,12 +64,12 @@ fn parse(args: Vec<String>){
                         while i < args.len(){
                             match &args[i][..] {
                                 "-o"  =>  {
-                                    let points: (Option<image::Point>, Option<image::Point>) = (Some(image::Point{x:0, y:0}), Some(image::Point{x:2500, y:100}));
-                                    let compressed_images = image::run(None, Some(1), points);
+                                    let points: (Option<image::Point>, Option<image::Point>) = (Some(image::Point{x:10, y:500}), Some(image::Point{x:500, y:100}));
+                                    let compressed_images = image::run(None, points);
                                     let mut k = 0;
                                     for image in compressed_images {
                                         // TODO: make option and unwrap or for default file location
-                                        fs::write(format!("{}/{}.png",&args[i+1][..], k), image).unwrap();
+                                        fs::write(format!("target/{}.png", k), image).unwrap();
                                         k = k + 1;
                                     }
                                     i = i+1;
