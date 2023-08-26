c980f19c016684ae3b6b81be2983097c9dca2ee3
James Eginton <jwe66@cornell.edu>
2023-08-26 17:47:14 -0500

    drawing rectangles from corner to cursor.
    untested drawing of rectangles form 0,0 to the cursor while holding left click.

--- src/args/gui.rs
+++ src/args/gui.rs
@@ -1,23 +1,30 @@
-use iced::window::{Position, Icon, Level};
+use iced::window::{Position,Level};
 use iced::font::{self, Font};
-use iced::{ executor, alignment, Alignment, Command, Length, window, Application, Settings, Subscription, Point };
-use iced::widget:: {checkbox, column, container, text, mouse_area};
+use iced:: { 
+    executor, alignment, Alignment, Command, Length, 
+    window, Settings, Subscription, Point, Application
+};
+use iced::widget:: {column, container};
 // use iced::mouse::{self, Event};
 use iced::mouse;
 use iced::subscription;
 use iced::Event;
+use iced::widget::canvas::{self, Canvas, Frame, Geometry, Path, Text};
+use crate::args::image_proc;
 
 use self::theme::Theme;
 use self::widget::Element;
-
+use iced::Rectangle;
 
 const ICON_FONT: Font = Font::with_name("icons");
 
+
+
 #[derive(Debug, Clone)]
 enum Message {
-    onMousePressed,
-    onMouseMoved(Point),
-    onMouseReleased,
+    OnMousePressed,
+    OnMouseMoved(Point),
+    OnMouseReleased,
 }
 
 
@@ -26,7 +33,7 @@ pub fn run() {
         window: window::Settings {
             resizable: false,
             decorations: false,
-            position: Position::Specific((0i32), (0i32)),
+            position: Position::Specific(0i32, 0i32),
             visible: true,
             transparent: true,
             level: Level::AlwaysOnTop,
@@ -41,7 +48,19 @@ pub fn run() {
     App::run(settings);
 }
 
-struct App;
+#[derive(Debug, Clone, Default)]
+struct AppState {
+    rectnagle: Rectangle,
+    drawing: Option<Point>
+}
+
+struct App{
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+    pressed: bool,
+}
 
 impl Application for App {
     type Message = Message;
@@ -50,36 +69,51 @@ impl Application for App {
     type Flags = ();
 
     fn new(_flags: ()) -> (App, Command<Self::Message>) {
-        (App, window::change_mode(iced::window::Mode::Fullscreen))
+        (
+            App {
+                x: 0f32,
+                y: 0f32,
+                width: 10f32,
+                height: 10f32,
+                pressed: false
+            }, 
+            window::change_mode(iced::window::Mode::Fullscreen)
+        )
     }
     fn title(&self) -> String {
         String::from("Hi")
     }
-    fn update(&mut self, _message: Message) -> Command<Message> {
+    fn update(&mut self, _message: Message) -> Command<Message>{
         match _message {
 
-            Message::onMousePressed => {
+            Message::OnMousePressed => {
                 println!("mouse pressed");
+                self.x = 0f32;
+                self.y = 0f32;
+                self.pressed = true;
                 Command::none()
             }
 
-            Message::onMouseMoved(_point) => {
+            Message::OnMouseMoved(_point) => {
                 println!("mouse moved to {:?}", _point);
+                if self.pressed {
+                    self.width = _point.x;
+                    self.height = _point.y;
+                }
                 Command::none()
             }
 
-            Message::onMouseReleased => {
+            Message::OnMouseReleased => {
                 println!("mouse released");
+                self.pressed = false;
                 Command::none()
             }
 
-            _ => {
-                Command::none()
-            }
+            _ => {Command::none()}
         }
     }
     fn view(&self) -> Element<Message> {
-        let content = column![];
+        /*let content = column![];
         container(content)
             .width(Length::Fill)
             .height(Length::Fill)
@@ -87,20 +121,31 @@ impl Application for App {
             .center_y()
             .padding(0)
             .style(theme::Container::Bordered)
-            .into()    
+            .into() */
+        let content = column![
+            rectangle::rectangle(self.x, self.y, self.width, self.height),
+        ]
+        .padding(0)
+        .spacing(0)
+        .align_items(Alignment::Center);
+
+        container(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
     }
 
     fn subscription(&self) -> Subscription<Message> {
         subscription::events_with(|event, _status| {
             match event {
                 Event::Mouse(mouse::Event::ButtonPressed(mouse::Button::Left)) => {
-                    Some(Message::onMousePressed)
+                    Some(Message::OnMousePressed)
                 }
                 Event::Mouse(mouse::Event::CursorMoved { position }) => {
-                    Some(Message::onMouseMoved(position))
+                    Some(Message::OnMouseMoved(position))
                 }
                 Event::Mouse(mouse::Event::ButtonReleased(mouse::Button::Left)) => {
-                    Some(Message::onMouseReleased)
+                    Some(Message::OnMouseReleased)
                 }
                 _ => None
             }
@@ -153,3 +198,137 @@ mod theme {
         }
     }
 }
+
+mod rectangle {
+    use iced::advanced::layout::{self, Layout};
+    use iced::advanced::renderer;
+    use iced::advanced::widget::{self, Widget};
+    use iced::mouse;
+    use iced::{Color, Element, Length, Size};
+    
+    pub struct Rectangle {
+        x: f32,
+        y: f32,
+        width: f32,
+        height: f32,
+    }
+
+    impl Rectangle {
+        pub fn new(x: f32, y: f32, width: f32, height: f32) -> Self {
+            Self { 
+                x,
+                y,
+                width,
+                height,
+            }
+        }
+    }
+
+    pub fn rectangle(x: f32, y: f32, width: f32, height: f32) -> Rectangle {
+        Rectangle::new(x, y, width, height)
+    }
+
+    impl<Message, Renderer> Widget<Message, Renderer> for Rectangle 
+        where
+            Renderer: renderer::Renderer,
+        {
+            fn width(&self) -> Length {
+                Length::Shrink
+            }
+
+            fn height(&self) -> Length {
+                Length::Shrink
+            }
+
+            fn layout(
+                &self,
+                _renderer: &Renderer,
+                _limits: &layout::Limits,
+                ) -> layout::Node {
+                layout::Node::new(Size::new(self.width, self.height))
+            }       
+            fn draw(
+                &self,
+                _state: &widget::Tree,
+                renderer: &mut Renderer,
+                _theme: &Renderer::Theme,
+                _style: &renderer::Style,
+                layout: Layout<'_>,
+                _cursor: mouse::Cursor,
+                _viewport: &iced::Rectangle,
+                ) {
+                renderer.fill_quad(
+                    renderer::Quad {
+                        bounds: layout.bounds(),
+                        border_radius: 2.0.into(),
+                        border_width: 0.0,
+                        border_color: Color::TRANSPARENT,
+                    },
+                    Color::WHITE,
+                );
+            }
+        }
+    impl<'a, Message, Renderer> From<Rectangle> for Element<'a, Message, Renderer>
+    where
+        Renderer: renderer::Renderer,
+    {
+        fn from(rectangle: Rectangle) -> Self {
+            Self::new(rectangle)
+        }
+    }
+
+}
+
+/*mod rectangle {
+  use iced::mouse;
+  use iced::widget::canvas::event::{self, Event};
+  use iced::widget::canvas::{self, Canvas, Frame, Geometry, Path, Stroke};
+  use iced::{Element, Length, Point, Renderer, Theme};
+
+  #[derive(Default)]
+  pub struct State {
+  cache: canvas::Cache,
+  }
+
+  impl State {
+  pub fn view<'a>(&'a self, rectangle: &'a iced::Rectangle) -> Element<'a, Rectangle> {
+  let canvas = Canvas::new(Rectangle {
+  state: self,
+  Rectangle: rectangle,
+  });
+  canvas.width(Length.Fill);
+  canvas.height(Length.Fill);
+  canvas.into()
+  }
+
+
+  }
+  struct Rectangle<'a> {
+  state: &'a State,
+  Rectangle: &'a iced::Rectangle,
+  }
+
+  impl<'a> canvas::Program<iced::Rectangle> for Rectangle<'a> {
+  type State = Option<Pending>;
+  fn update(
+  &self,
+  state: &mut Self::State,
+  event: Event,
+  bounds: Rectangle,
+  cursor: mouse::Cursor,
+  ) -> (event::Status, Option<iced::Rectangle>) {
+  let cursor_position =
+  if let Some(position) = cursor.position_in(bounds) {
+  position
+  } else {
+  return (event::Status::Ignored, None);
+  };
+  match event {
+  Event::Mouse(mouse_event) => {
+  let message = match mouse_event {
+  mouse::Event::ButtonPressed(mouse::Button::Left) => {
+  match *state 
+  }
+  }
+  }
+  }*/
