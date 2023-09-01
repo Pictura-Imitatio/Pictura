d3993776daf59a4b96a57ea5d815e6a5cca81af5
Luxzi <lesson085@gmail.com>
2023-09-01 15:18:31 -0400

    Change logger to use RUST_LOG env var

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod gui;
 use std::env;
 
 fn main() {
-    simple_logger::init_with_level(log::Level::Info).unwrap();
+    simple_logger::init_with_env().unwrap();
 
     let args: Vec<String> = env::args().skip(1).collect();
     args::parse(args);
