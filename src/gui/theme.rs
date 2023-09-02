1993e6927aa97fcf9ca8a06fa6f316b3bb0ad023
James Eginton <jwe66@cornell.edu>
2023-09-02 17:49:19 -0500

    minor code cleaning in the gui.rs file and gui folder

--- src/gui/theme.rs
+++ src/gui/theme.rs
@@ -1,6 +1,6 @@
 pub mod widget {
     #![allow(dead_code)]
-    use crate::gui::theme::theme::Theme;
+    use crate::gui::theme::Theme;
     pub type Renderer = iced_wgpu::Renderer<Theme>;
     pub type Element<'a, Message, Renderer> = iced_winit::core::Element<'a, Message, Renderer>;
     pub type Container<'a, Message> = iced::widget::Container<'a, Message, Renderer>;
@@ -8,40 +8,37 @@ pub mod widget {
     pub type Cursor = iced::mouse::Cursor;
 }
 
-pub mod theme {
-    use iced::widget::container;
-    use iced::{ application, color };
+use iced::widget::container;
+use iced::{ application, color };
 
-    #[derive(Debug, Clone, Copy, Default)]
-    pub struct Theme;
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Theme;
 
-    impl application::StyleSheet for Theme {
-        type Style = ();
-        fn appearance(&self, _style: &Self::Style) -> application::Appearance {
-            application::Appearance {
-                background_color: color!(0x28, 0x28, 0x28, 0.7),
-                text_color: color!(0xeb, 0xdb, 0xb2),
-            }
-        }
-    }
-    #[derive(Debug, Clone, Copy, Default)]
-    pub enum Container {
-        #[default]
-        Default,
-        Bordered,
-    }
-    impl container::StyleSheet for Theme {
-        type Style = Container;
-
-        fn appearance(&self, style: &Self::Style) -> container::Appearance {
-            match style {
-                Container::Default => container::Appearance::default(),
-                Container::Bordered => container::Appearance {
-                    border_color: color!(0x45, 0x85, 0x88),
-                    ..Default::default()
-                },
-            }
+impl application::StyleSheet for Theme {
+    type Style = ();
+    fn appearance(&self, _style: &Self::Style) -> application::Appearance {
+        application::Appearance {
+            background_color: color!(0x28, 0x28, 0x28, 0.7),
+            text_color: color!(0xeb, 0xdb, 0xb2),
         }
     }
 }
+#[derive(Debug, Clone, Copy, Default)]
+pub enum Container {
+    #[default]
+    Default,
+    Bordered,
+}
+impl container::StyleSheet for Theme {
+    type Style = Container;
 
+    fn appearance(&self, style: &Self::Style) -> container::Appearance {
+        match style {
+            Container::Default => container::Appearance::default(),
+            Container::Bordered => container::Appearance {
+                border_color: color!(0x45, 0x85, 0x88),
+                ..Default::default()
+            },
+        }
+    }
+}
